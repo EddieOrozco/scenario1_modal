c357d0dcf0eef18de8edf22d8626b0dcc2c5f00c
EddieOrozco <orozcoe230@gmail.com>
2021-03-23 21:56:06 -0500

    Adding modal functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+// All Modal Events
 //  Adding function to window onload
 window.onload = function () {
+  let emailState = false;
   // Creating Variable from HTML Elements
   let emailModal = document.getElementsByClassName('email-modal')[0];
   let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
@@ -12,11 +14,20 @@ window.onload = function () {
     emailModal.classList.remove('email-modal--visible');
   });
 
+  // Created Function for event listener
+  let showModal = () => {
+    if (emailState === false) {
+      // Adding classList via JS to HTML Element
+      emailModal.classList.add('email-modal--visible');
+      emailState = true;
+    }
+  };
+
   // Adding event listener to the HTML document w/ attached function
   document.body.addEventListener('mouseleave', () => {
-    // Adding classList via JS to HTML Element
-    emailModal.classList.add('email-modal--visible');
-    console.log('mouse left');
+    // Trigged the Conditional Function
+    showModal();
   });
+
   console.log(document);
 };
